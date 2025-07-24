1e77d2d32e3fff04d33db950e8671cbe2f21622d
Eliandro <eliandro.gomes42@gmail.com>
2025-07-23 22:35:37 -0300

    UPDATE

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import model.Category;
 import model.MoneyMovement;
 
 
@@ -10,17 +11,23 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        Category category = new Category();
+        category.setDescription("Food");
+        // category.setDateChanged();
+
         MoneyMovement mov = new MoneyMovement();
         mov.setMovementDate(new Date());
-        mov.setDescription("Computer purchase");
+        mov.setDescription("Buying food");
         mov.setType("D");
         mov.setValue(5000.);
+        mov.setCategory(category);
 
         EntityManagerFactory emf=
                 Persistence.createEntityManagerFactory("persistenciaPU");
         EntityManager em =emf.createEntityManager();
         em.getTransaction().begin();
         em.persist(mov);
+        em.persist(category);
         em.getTransaction().commit();
         em.close();
         System.out.println(mov.getId());
