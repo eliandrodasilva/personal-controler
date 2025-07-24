2a88718b1a63454984e27631f637a0b47f685afd
Eliandro <eliandro.gomes42@gmail.com>
2025-07-23 22:31:09 -0300

    WIP: DAO for Category class

---
+++ src/main/java/dao/CategoryDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import model.Category;
+
+import java.util.List;
+
+public class CategoryDAO {
+    public Category insert() {
+
+    }
+
+    public Category change() {
+
+    }
+
+    public void remove() {
+
+    }
+
+    public Category searchFromID(Long id) {
+
+    }
+
+    public List<Category> searchAll() {
+
+    }
+}
