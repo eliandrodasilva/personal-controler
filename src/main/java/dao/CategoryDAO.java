36ee7d947e0fb865e001736f92165b5511232cde
Eliandro <eliandro.gomes42@gmail.com>
2025-08-06 22:25:29 -0300

    Implements CategoryDAO

--- src/main/java/dao/CategoryDAO.java
+++ src/main/java/dao/CategoryDAO.java
@@ -2,26 +2,54 @@ package dao;
 
 import model.Category;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
 public class CategoryDAO {
-    public Category insert() {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenciaPU");
 
+    public Category insert(Category category) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(category);
+        em.getTransaction().commit();
+        em.close();
+        return category;
     }
 
-    public Category change() {
-
+    // category objects needs id
+    public Category change(Category category) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(category);
+        em.getTransaction().commit();
+        em.close();
+        return category;
     }
 
-    public void remove() {
-
+    public void remove(Category category) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.remove(category);
+        em.getTransaction().commit();
+        em.close();
     }
 
     public Category searchFromID(Long id) {
-
+        EntityManager em = emf.createEntityManager();
+        Category category = em.find(Category.class, id);
+        em.close();
+        return category;
     }
 
     public List<Category> searchAll() {
-
+        EntityManager em = emf.createEntityManager();
+        Query query = em.createQuery("from Category");
+        List<Category> categoryList = query.getResultList();
+        em.close();
+        return categoryList;
     }
 }
