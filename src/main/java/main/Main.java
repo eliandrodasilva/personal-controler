cdb55e8accb157bd4be8b8f4ddacd6b5d1610fbb
Eliandro <eliandro.gomes42@gmail.com>
2025-08-06 22:26:01 -0300

    Updates main to use DAOs

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import dao.CategoryDAO;
+import dao.MovementDAO;
 import model.Category;
 import model.MoneyMovement;
 
@@ -8,28 +10,36 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        CategoryDAO categoryDAO = new CategoryDAO();
+        MovementDAO movementDAO = new MovementDAO();
+
         Category category = new Category();
-        category.setDescription("Food");
+        category.setDescription("Wealth");
         // category.setDateChanged();
+        categoryDAO.insert(category);
 
-        MoneyMovement mov = new MoneyMovement();
-        mov.setMovementDate(new Date());
-        mov.setDescription("Buying food");
-        mov.setType("D");
-        mov.setValue(5000.);
-        mov.setCategory(category);
+//        Category category1 = categoryDAO.searchFromID(2L);
+//        System.out.println(category1.getDescription());
+//        category1.setDescription("Minecraft");
+//        categoryDAO.change(category1);
+//        List<Category> categoryList = categoryDAO.searchAll();
+//        for(Category cat:categoryList) {
+//            System.out.println(cat.getDescription());
+//        }
 
-        EntityManagerFactory emf=
-                Persistence.createEntityManagerFactory("persistenciaPU");
-        EntityManager em =emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(mov);
-        em.persist(category);
-        em.getTransaction().commit();
-        em.close();
-        System.out.println(mov.getId());
+//        MoneyMovement movement = new MoneyMovement();
+//        movement.setMovementDate(new Date());
+//        movement.setDescription("Buying food");
+//        movement.setType("D");
+//        movement.setValue(5000.);
+//        movement.setCategory(category);
+
+        // movementDAO.insert(movement);
+
+//        System.out.println(movement.getId());
     }
 }
